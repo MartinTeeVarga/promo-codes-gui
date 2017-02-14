d4c00eb19da6659223c0443d8359be94d86d8de5
sm4 <sm4@kul.is>
2017-02-14 21:55:49 +1100

    Started working on tests

--- test/unit/specs/Grid.spec.js
+++ test/unit/specs/Grid.spec.js
@@ -1,13 +1,27 @@
-// import Vue from 'vue';
-// import Hello from 'src/components/Hello';
+import Vue from 'vue';
+import Grid from 'src/components/Grid';
 
-describe('GameDetails.vue', () => {
-  // it('should render correct contents', () => {
-  //   const vm = new Vue({
-  //     el: document.createElement('div'),
-  //     render: (h) => h(Hello)
-  //   });
-  //   expect(vm.$el.querySelector('.hello h1').textContent)
-  //     .to.equal('Welcome to Your Vue.js App');
-  // });
+function element (Component, propsData) {
+  const Ctor = Vue.extend(Component);
+  const vm = new Ctor({ propsData }).$mount();
+  return vm.$el;
+}
+
+describe('Grid.vue', () => {
+  it('should render column headers', () => {
+    const el = element(Grid, {
+      data: [],
+      columns: [
+        {key: 'a', name: 'ColumnA', filter: 'none'},
+        {key: 'b', name: 'ColumnB', filter: 'none'}
+      ],
+      initialSortKey: 'a'
+    });
+    expect(el.querySelector('thead th.active').textContent.trim())
+      .to.equal('ColumnA');
+    expect(el.querySelector('thead th.active span').className)
+      .to.equal('fa fa-caret-up');
+    expect(el.querySelector('thead th:not(.active)').textContent.trim())
+      .to.equal('ColumnB');
+  });
 });
