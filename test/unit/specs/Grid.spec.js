9dc0fd8207cefbbec90e102b78e55119f926b11a
sm4 <sm4@kul.is>
2017-02-15 22:38:43 +1100

    Unit test todo

--- test/unit/specs/Grid.spec.js
+++ test/unit/specs/Grid.spec.js
@@ -1,15 +1,14 @@
 import Vue from 'vue';
 import Grid from 'src/components/Grid';
 
-function element (Component, propsData) {
+function create (Component, propsData) {
   const Ctor = Vue.extend(Component);
-  const vm = new Ctor({ propsData }).$mount();
-  return vm.$el;
+  return new Ctor({ propsData }).$mount();
 }
 
 describe('Grid.vue', () => {
   it('should render column headers', () => {
-    const el = element(Grid, {
+    const vm = create(Grid, {
       data: [],
       columns: [
         {key: 'a', name: 'ColumnA', filter: 'none'},
@@ -17,11 +16,23 @@ describe('Grid.vue', () => {
       ],
       initialSortKey: 'a'
     });
-    expect(el.querySelector('thead th.active').textContent.trim())
+    expect(vm.$el.querySelector('thead th.active').textContent.trim())
       .to.equal('ColumnA');
-    expect(el.querySelector('thead th.active span').className)
+    expect(vm.$el.querySelector('thead th.active span').className)
       .to.equal('fa fa-caret-up');
-    expect(el.querySelector('thead th:not(.active)').textContent.trim())
+    expect(vm.$el.querySelector('thead th:not(.active)').textContent.trim())
       .to.equal('ColumnB');
+  });
+  it('should debounce search', () => {
+    const vm = create(Grid, {
+      data: [],
+      columns: [
+        {key: 'a', name: 'ColumnA', filter: 'none'},
+        {key: 'b', name: 'ColumnB', filter: 'none'}
+      ],
+      initialSortKey: 'a'
+    });
+    var box = vm.$el.querySelector('#filterBox');
+    //TODO figure out how to send keys
   });
 });
