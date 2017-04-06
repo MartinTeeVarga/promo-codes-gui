d9958ab811bc8373cdce16987ed4ef630f60bd14
sm4 <sm4@kul.is>
2017-04-06 20:50:12 +1000

    testing data

--- src/components/Promocodes.vue
+++ src/components/Promocodes.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="row">
     <div class="row">
-      <label for="games">Reason for contacting</label>
+      <label for="games">Games</label>
       <select class="u-full-width" id="games">
-        <option value="GAME0">Game 1</option>
-        <option value="GAME1">Game 2</option>
-        <option value="GAME2">Game 3</option>
+        <option value="GAME-1">Game 1</option>
+        <option value="GAME-2">Game 2</option>
+        <option value="GAME-3">Game 3</option>
       </select>
     </div>
     <div class="row">
@@ -36,7 +36,7 @@
           {key: 'codeId', name: 'Code', filter: 'none'},
           {key: 'from', name: 'From', filter: 'localDate'},
           {key: 'to', name: 'To', filter: 'localDate'},
-          {key: 'pub', name: 'Public', filter: 'none'}],
+          {key: 'pub', name: 'Public', filter: 'boolean'}],
         gridData: [],
         sortKey: 'codeId'
       }
@@ -44,7 +44,7 @@
     methods: {
       fetch: function () {
         var self = this
-        axios.get(process.env.API_URL + '/games/GAME0/codes/list')
+        axios.get(process.env.API_URL + '/games/GAME-1/codes/list')
           .then(function (response) {
             // todo deal with errors
             self.gridData = response.data
