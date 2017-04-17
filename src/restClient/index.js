2689b00463d182c88c4819adec5a4c3d06289d1c
sm4 <sm4@kul.is>
2017-04-17 21:25:45 +1000

    added createCode method

--- src/restClient/index.js
+++ src/restClient/index.js
@@ -15,12 +15,24 @@ Rc.prototype.setToken = function (token) {
 Rc.prototype.deleteCode = function (gameId, codeId) {
   return new Promise((resolve, reject) => {
     axios.delete(process.env.API_URL + '/games/' + gameId + '/codes/' + codeId)
-      .then(function (response) {
-        resolve('Code ' + gameId + ' deleted.')
+      .then(function () {
+        resolve('Code ' + codeId + ' deleted.')
       })
       .catch(function (error) {
         console.error(error)
         reject('Error while deleting code ' + codeId)
+      })
+  })
+}
+Rc.prototype.createCode = function (code) {
+  return new Promise((resolve, reject) => {
+    axios.post(process.env.API_URL + '/games/' + code.gameId + '/codes', code)
+      .then(function () {
+        resolve('Code ' + code.codeId + ' created.')
+      })
+      .catch(function (error) {
+        console.error(error)
+        reject('Error while creating code ' + code.codeId)
       })
   })
 }
