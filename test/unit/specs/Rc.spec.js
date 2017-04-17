9973d8c1d9c24e7ec7c81c054df627a3f84cefbf
sm4 <sm4@kul.is>
2017-04-17 21:26:02 +1000

    added test

--- test/unit/specs/Rc.spec.js
+++ test/unit/specs/Rc.spec.js
@@ -21,19 +21,49 @@ describe.only('Rest Client', () => {
       moxios.stubRequest(process.env.API_URL + '/games/GM01/codes/CD01', {
         status: 200
       })
+      rc.setToken('Token')
       rc.deleteCode('GM01', 'CD01')
         .then(function (message) {
           expect(message).to.contain('CD01')
+          let request = moxios.requests.mostRecent()
+          expect(request.config.method).to.equal('delete')
+          expect(request.config.headers['X-Token']).to.equal('Token')
           done()
         })
     })
-    it('should catch errors', function (done) {
+    it('should catch errors', done => {
       moxios.stubRequest(process.env.API_URL + '/games/GM01/codes/CD01', {
         status: 400
       })
       rc.deleteCode('GM01', 'CD01')
         .catch(function (message) {
           expect(message).to.contain('CD01')
+          done()
+        })
+    })
+  })
+
+  describe('createCode', function () {
+    it('should create new code', done => {
+      moxios.stubRequest(process.env.API_URL + '/games/GM01/codes', {
+        status: 200
+      })
+      var code = {
+        gameId: 'GM01',
+        codeId: 'CD01',
+        from: '1',
+        to: '2',
+        pub: true,
+        payload: 'empty'
+      }
+      rc.setToken('Token')
+      rc.createCode(code)
+        .then(function (message) {
+          expect(message).to.contain('CD01')
+          let request = moxios.requests.mostRecent()
+          expect(request.config.method).to.equal('post')
+          expect(request.config.headers['X-Token']).to.equal('Token')
+          expect(JSON.parse(request.config.data)).to.deep.equal(code)
           done()
         })
     })
